d06646f24ef7afd371a0ba8d8658a38f5cdca870
lucylee <lucylee@localhost>
2023-11-01 14:19:14 -0500

    yayyyy

--- src/studio7/Fraction.java
+++ src/studio7/Fraction.java
@@ -2,9 +2,16 @@ package studio7;
 
 public class Fraction {
 
+	private int numerator;
+	private int denominator;
+	public Fraction(int initNumerator, int initDenominator) {
+		numerator = initNumerator;
+		denominator = initDenominator;
+	}
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		
 	}
 
 }
